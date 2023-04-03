e97d3b01159886ab10534c5b9bce4324e8495aad
anuroopkanna <anuroopindia16@gmail.com>
2023-04-03 23:40:35 +0530

    firststart

---
+++ main/java/com/BridgeLabz/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public boolean validateFirstName(String value) {
+
+    }
+}
---
+++ test/java/com/BridgeLaz/EmailTest.java
@@ -0,0 +1,4 @@
+package com.BridgeLaz;
+
+public class EmailTest {
+}
