9a6534b225bb3176ed1e681151a5b923e3b5b259
anuroopkanna <anuroopindia16@gmail.com>
2023-04-04 00:07:23 +0530

    starts

--- main/java/com/BridgeLabz/UserRegistration.java
+++ main/java/com/BridgeLabz/UserRegistration.java
@@ -5,6 +5,11 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public boolean validateFirstName(String value) {
-
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Matcher matcher = pattern.matcher(value);
+        if (matcher.matches())
+            return true;
+        else
+            return false;
     }
 }
--- test/java/com/BridgeLaz/UserRegistrationTest.java
+++ test/java/com/BridgeLaz/UserRegistrationTest.java
@@ -1,4 +1,40 @@
 package com.BridgeLaz;
 
+import com.BridgeLabz.UserRegistration;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+
 public class UserRegistrationTest {
+    static UserRegistration userRegistration;
+
+    @BeforeAll
+    public static void init() {
+        System.out.println("Before all");
+        userRegistration = new UserRegistration();
+        @Test
+        void givenFirstnameshouldreturnTrue () {
+            boolean value = userRegistration.validateFirstName("Anuroop");
+            Assertions.assertTrue(value);
+            System.out.println("First Name Checked:- Successfully passed UC1");
+        }
+        @Test
+        void givenLastNameShouldReturnTrue () {
+            boolean LastName = userRegistration.validateLastname("Pulluru");
+            Assertions.assertTrue(LastName);
+            System.out.println("Last Name Checked :- Successfully passed UC2");
+        }
+        @Test
+        void givenEmailidShouldReturnTrue () {
+            boolean Email = userRegistration.validateEmail("anuroop@gmail.com");
+            Assertions.assertTrue(Email);
+            System.out.println("Email ID Checked :- Successfully Passed UC3");
+        }
+        @Test
+        void givenMobileNumberShouldReturnTrue () {
+            boolean PhoneNumber = userRegistration.validatPhoneNumber("919032939140");
+            Assertions.assertTrue(PhoneNumber);
+            System.out.println("PhoneNumber Checked :- Successfully Passed UC4");
+        }
+    }
 }
